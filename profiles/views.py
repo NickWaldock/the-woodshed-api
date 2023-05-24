cee55e87d7686463bd2ea7786ed46c8e3d1b2401
NickWaldock <nickwaldock@me.com>
2023-05-24 15:38:47 +0000

    Add edit profile ability with dj-rest

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -20,6 +21,8 @@ class ProfileDetail(APIView):
     """
     Displays the details of a single profile instance
     """
+    serializer_class = ProfileSerializer
+    
     def get_object(self, pk):
         try: 
             profile = Profile.objects.get(pk=pk)
@@ -31,3 +34,12 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
