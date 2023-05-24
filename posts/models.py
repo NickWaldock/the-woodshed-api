fbefbd52fbdeabf578c532dd70a0806a0e447296
NickWaldock <nickwaldock@me.com>
2023-05-24 18:12:05 +0000

    Create Post model and posts app

---
+++ posts/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.model):
+    """
+    Post model related to the user (owner) instance
+    """
+    owner = models.ForeignKey(User, on_delete=CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    instrument = models.CharField(max_length=255)
+    tags = models.CharField(max_length=255)
+    file = models.FileField(
+        upload_to='../woodshed_media/pdfs/',
+        blank=True,
+        default='../woodshed_media/pdf-default_kpbimk.png'
+    )
+    
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
