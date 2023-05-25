fd55db0c925db80ac3c583ae1a63e097cbd05f67
NickWaldock <nickwaldock@me.com>
2023-05-25 11:33:52 +0000

    Build CommentList view

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,13 @@
 from rest_framework import generics, permissions
 from woodshed_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
-from .serializers import CommentSerializer
+from .serializers import CommentSerializer, CommentDetailSerializer
 
 
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    query_set = Comment.objects.all()
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
