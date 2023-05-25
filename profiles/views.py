dbb5f1653623979716eeda0b005be99290e439e1
NickWaldock <nickwaldock@me.com>
2023-05-25 18:37:39 +0000

    Add DjangoFilterBackends and filtersets to profiles/views.py, updated requirements.txt

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from woodshed_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -16,13 +17,19 @@ class ProfileList(generics.ListAPIView):
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
     ordering_fields = [
         'posts_count', 
         'followers_count',
         'following_count',
         'owner__following__created_at',
         'owner__followed__created_at',
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
     ]
 
 
