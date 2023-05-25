f0ed5a9265c1a638350fda5cc00d8b0ba9fe8f63
NickWaldock <nickwaldock@me.com>
2023-05-25 16:23:12 +0000

    Create FollowerList view

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from woodshed_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    permission_class = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
