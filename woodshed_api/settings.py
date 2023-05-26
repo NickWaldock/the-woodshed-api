b44b00d9bc3815388c217917fc5ce46beca92d0d
NickWaldock <nickwaldock@me.com>
2023-05-26 19:38:48 +0000

    Fix typo in settings.py secret key

--- woodshed_api/settings.py
+++ woodshed_api/settings.py
@@ -56,7 +56,7 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SERCRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
