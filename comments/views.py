7beafbdd1275d554559e8f15ee8e10f26a8cf335
NickWaldock <nickwaldock@me.com>
2023-05-26 16:25:48 +0000

    add extra ordering field to profiles/views.py and add djangofilterbackends plus filterset to comments/views.py

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django.rest_framework import DjangoFilterBackend
 from woodshed_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -11,6 +12,7 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
