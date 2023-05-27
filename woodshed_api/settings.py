e0b19deb13b2b25c9358b3c972296bcd5d545790
NickWaldock <nickwaldock@me.com>
2023-05-27 11:34:49 +0000

    Bug1 fix attempt for depolyment

--- woodshed_api/settings.py
+++ woodshed_api/settings.py
@@ -155,8 +155,6 @@ else:
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
--- woodshed_api/views.py
+++ woodshed_api/views.py
@@ -5,11 +5,13 @@ from .settings import (
     JWT_AUTH_SAMESITE, JWT_AUTH_SECURE,
 )
 
+
 @api_view()
 def root_route(request):
     return Response({
         "message": "Welcome to The Woodshed API!"
     })
+
 
 # dj-rest-auth logout view fix
 @api_view(['POST'])
